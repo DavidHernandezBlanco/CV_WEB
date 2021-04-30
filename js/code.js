37dd930a1153b5868b575d4fc4c7879f1cb67de3
David <7338-joan23@fje.edu>
2021-04-30 15:13:19 +0200

    CV_WEB

---
+++ js/code.js
@@ -0,0 +1,6 @@
+function language() {
+    var idioma = document.getElementById('language').value
+    if (idioma == 'ES') {
+        window.location.href = '../index.html'
+    } else {
+        window.location.href = './en/index.html'
